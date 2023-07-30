705152ed7e02747414e1a720f2e41b7c6d22ad39
Hek-tor <EmilioCmDz@gmail.com>
2023-07-30 15:16:58 -0600

    Inicio de proyecto

---
+++ js/Scroll.js
@@ -0,0 +1,21 @@
+document.getElementById("flechabtn").style.display = "none";
+
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 280 || document.documentElement.scrollTop > 280) {
+        document.getElementById("flechabtn").style.display = "block";
+    } else {
+        document.getElementById("flechabtn").style.display = "none";
+    }
+}
+
+
+function topFunction() {
+    $('a.scroll-top').click(function (event) {
+        event.preventDefault();
+        $('html, body').animate({ scrollTop: 0 });
+    });
+}
